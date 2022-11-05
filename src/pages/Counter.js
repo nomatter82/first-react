d34ddfb7bf63eea161a5d4ffcfee40c376423038
nomatter82 <mirthful82@gmail.com>
2022-11-05 17:35:27 +0900

    first

---
+++ src/pages/Counter.js
@@ -0,0 +1,29 @@
+import HeadLessLayout from "components/layouts/HeadLessLayout";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Counter = () => {
+  const navigate = useNavigate();
+
+  const [num, setNum] = useState(0);
+
+  const increase = () => setNum((prev) => prev + 1);
+
+  return (
+    <HeadLessLayout>
+      <div>
+        <div>카운터페이지</div>
+
+        <div>클릭수 {num}</div>
+        <div>
+          <button onClick={increase}>클릭</button>
+        </div>
+
+        {/* <Link to={"/"}>메인으로 이동</Link> */}
+        <button onClick={() => navigate("/")}>메인으로 이동</button>
+      </div>
+    </HeadLessLayout>
+  );
+};
+
+export default Counter;
